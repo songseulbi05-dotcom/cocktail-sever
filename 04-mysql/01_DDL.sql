399e9d58adb9e49cf9023048265f34839e7a022c
songseulbi05-dotcom <songseulbi05@gmail.com>
2026-03-19 09:37:58 +0900

    cocktail

---
+++ 04-mysql/01_DDL.sql
@@ -0,0 +1,10 @@
+/*
+    - 데이터(data) : 화면에 보이거나, 사용자가 입력하거나, 저장해야하는 정보 
+    - 데이터베이스(database) : 데이터를 저장하고 필요할 때 꺼내 쓰는 공간 
+    - DBMS(database mangent system) : 데이터를 관리하는 프로그램 
+    - RDBMS(relational database mangent system) : 관계형 데이터 베이스 관리 시스템
+        예) MySQL, Oracle, PostgreSQL
+    
+    - 엔티티(Entity) : 같은 성격을 가진 데이터를 묶어놓는 큰 주제
+    
+*/
